d965eabf2ba27515ddcf72fcdace7f7013619cad
OtabekBobomurodov <bobomurodov.otabek@mail.ru>
2021-02-03 22:03:27 +0500

    first commit

---
+++ src/main/java/com/example/careerapp/model/User.java
@@ -0,0 +1,13 @@
+package com.example.careerapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String userName;
+    private String role;
+}
